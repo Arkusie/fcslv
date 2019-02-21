5abe90560d3d8eecdc4d66bc20f217bf3f81ed5b
Arek <majkhrhr@gmail.com>
2019-02-21 15:13:48 +0100

    content, toggle galleries

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,17 +1,34 @@
 const menuButton = document.getElementById("menu-container");
-menuButton.addEventListener("click", ToggleMenu);
 
-// const droneBtn = document.getElementById("droneBtn");
-// const cameraBtn = document.getElementById("cameraBtn");
+const droneBtn = document.getElementById("droneBtn");
+const cameraBtn = document.getElementById("cameraBtn");
 // const aboutBtn = document.getElementById("aboutBtn");
-const navMenu = document.getElementById("nav");
+const navMenu = document.getElementById("nav-list");
 const menuStripe1 = document.getElementById("stripe1");
 const menuStripe2 = document.getElementById("stripe2");
 const menuStripe3 = document.getElementById("stripe3");
 
-droneBtn.addEventListener("click", activeDroneContent);
+const droneContent = document.getElementById("grid-drone");
+const cameraContent = document.getElementById("grid-camera");
 
-function activeDroneContent() {
+cameraContent.style.display = "none";
+// listeners
+droneBtn.addEventListener("click", droneContentDisplay);
+cameraBtn.addEventListener("click", cameraContentDisplay);
+menuButton.addEventListener("click", ToggleMenu);
+//
+
+function droneContentDisplay() {
+  droneBtn.classList.add("current-page");
+  cameraBtn.classList.remove("current-page");
+  cameraContent.style.display = "none";
+  droneContent.style.display = "";
+}
+function cameraContentDisplay() {
+  droneBtn.classList.remove("current-page");
+  cameraBtn.classList.add("current-page");
+  droneContent.style.display = "none";
+  cameraContent.style.display = "";
   console.log("kek");
 }
 
